d570ffcf861d1382be5710cbd5c331c15d706383
Raphael Rodrigues dos Santos <raphaelrdgss.006@gmail.com>
2025-04-15 20:01:39 -0300

    Adding the database configuration in the app

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,27 @@
-﻿using TodoList_Flowmodoro.Views;
+﻿using TodoList_Flowmodoro.Helper;
+using TodoList_Flowmodoro.Views;
+using NavigationPage = Microsoft.Maui.Controls.NavigationPage;
 
 namespace TodoList_Flowmodoro;
 
 public partial class App : Application
 {
+    private static SQLiteDatabaseHelper _db;
+
+    public static SQLiteDatabaseHelper DB
+    {
+        get
+        {
+            if (_db == null)
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_todo.db3");
+                _db = new SQLiteDatabaseHelper(path);
+            }
+            
+            return _db;
+        }
+    }
+    
     public App()
     {
         InitializeComponent();
@@ -12,6 +30,6 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         //return new Window(new AppShell());
-        return new Window(new TodoList());
+        return new Window(new NavigationPage(new TodoList()));
     }
 }
--- Helper/SQLiteDatabaseHelper.cs
+++ Helper/SQLiteDatabaseHelper.cs
@@ -1,8 +1,42 @@
 using System;
+using SQLite;
+using TodoList_Flowmodoro.Models;
 
 namespace TodoList_Flowmodoro.Helper;
 
 public class SQLiteDatabaseHelper
 {
+    readonly SQLiteAsyncConnection _conn;
+    public SQLiteDatabaseHelper(string path)
+    {
+        _conn = new SQLiteAsyncConnection(path);
+        _conn.CreateTableAsync<TodoItem>().Wait();
+    }
 
+    public Task<int> Insert(TodoItem item)
+    {
+        return _conn.InsertAsync(item);
+    }
+
+    public Task<List<TodoItem>> Update(TodoItem item)
+    {
+        string sql = "UPDATE TodoItem set Title=?, set Description=? WHERE Id = ?";
+        return _conn.QueryAsync<TodoItem>(sql, item.Title, item.Description, item.Id);
+    }
+
+    public Task<int> Delete(int id)
+    {
+        return _conn.Table<TodoItem>().DeleteAsync(i => i.Id == id);
+    }
+
+    public Task<List<TodoItem>> GetAll()
+    {
+        return _conn.Table<TodoItem>().ToListAsync();
+    }
+
+    public Task<List<TodoItem>> Search(string q)
+    {
+        string sql = "SELECT * TodoItem WHERE description LIKE '%" + q + "%'";
+        return _conn.QueryAsync<TodoItem>(sql);
+    }
 }
